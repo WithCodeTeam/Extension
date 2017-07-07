e49f8e05ab3801eba584c65db90c2292c6674a47
Michał <kwestorowski.m@gmail.com>
2017-07-07 18:07:14 +0200

    need to fix

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,15 @@
 var findExtButton = document.getElementById('extButton')
 var findExtInput = document.getElementById('extInput')
 function updateInput(event) {
-  findExtInput.setAttribute("value", event.target.value)
+    findExtInput.setAttribute('value', event.target.value)
 }
 
-document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded', function () {
     findExtInput.onchange = updateInput;
-},false);
+}, false);
 
-var testF = function() {
-  console.log(findExtInput.value)
+var testF = function () {
+    console.log(findExtInput.value)
 }
 
 findExtButton.addEventListener('click', testF);
