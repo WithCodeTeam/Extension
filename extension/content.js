bc0caf16d25009b2189a034b63821f943cb09e6c
Michał <kwestorowski.m@gmail.com>
2017-07-09 09:42:58 +0200

    Fixed

--- extension/content.js
+++ extension/content.js
@@ -1,2 +1,2 @@
-document.body.style.backgroundColor = "red";
-document.getElementsByName('pesel').setAttribute('value', 'Wojtek')
+document.body.style.backgroundColor = "blue";
+var x = document.getElementsByName('pesel')[0].value='929392329'
