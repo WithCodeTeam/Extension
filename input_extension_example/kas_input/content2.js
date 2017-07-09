690c55d1994145fb79a26a3ed83bd5b1af98f5ed
Wojtek <kas@post.pl>
2017-07-09 10:25:38 +0200

    popup with pesel input and passing value to mtu

--- input_extension_example/kas_input/content2.js
+++
@@ -1,8 +0,0 @@
-console.log("target content2 ran");
-
-var storedLegal = chrome.storage.local.get('newStorage', function (items) {
-    console.log(items);
-
-    document.getElementById("firstName").value = items.newStorage.src_fname;
-    document.getElementById("lastName").value = items.newStorage.src_lname;
-});
