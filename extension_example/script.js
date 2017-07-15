e11a02504a7205e4044bf22e4a69f6ce530b9381
user <ravpaluch@gmail.com>
2017-07-15 13:38:06 +0200

    making a simple ext saving text in storage

---
+++ extension_example/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('save').onclick = function () {
+        var value = document.getElementById('saveLine').value;
+        alert(value);
+    }
+}
