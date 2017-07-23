9d3ee4918c23a2ee49d0b210625b622d3bef45af
Wojtek <kas@post.pl>
2017-07-23 16:10:06 +0200

    trying to send text from mtu options

--- extension/mtu.js
+++ extension/mtu.js
@@ -13,14 +13,13 @@ chrome.runtime.onMessage.addListener(
         var kilometersMtu = [25000, 75000, 125000, 175000, 225000]
         kilometersValue < 200001 ? kilometersActive = kilometersMtu[parseInt((kilometersValue / 50000 + '').charAt(0))] : kilometersActive = kilometersMtu[4]
 
-        var brandMtu = document.getElementsByName('etxBrand')[0].options
+        var brandMtu = function() {
+                for (var i = 0; i < document.getElementsByName('etxBrand')[0].options.length; i++ ) {
+                    document.getElementById('brand').innerHTML(document.getElementsByName('etxBrand')[0].options[i].text)
+                }}
 
-        // chrome.tabs.sendMessage({greeting: "hello"}, function(response) {
-        //     console.log(brandMtu);
-        // });
 
-
-        sendResponse({response: brandMtu});
+        sendResponse({response: brandMtu()});
         document.getElementsByName('pesel')[0].value=peselValue
         document.getElementsByName('name')[0].value=nameValue
         document.getElementsByName('surname')[0].value=surnameValue
